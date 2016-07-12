0cf7912c1b545420e5856dbb55a723b6dc3fd9dd
Roque Junior <roquejunior@roquejunior.com>
2016-07-12 15:35:01 -0300

    files

---
+++ js/yellowPagesController.js
@@ -0,0 +1,39 @@
+app.controller('yellowPagesController', ['$scope', function($scope){
+	$scope.app = 'Lista Telefonica';
+	$scope.contacts = [
+		{name: 'Roque', phone: '9999-9999'},
+		{name: 'Jonas', phone: '8888-8888'},
+		{name: 'Carlos', phone: '6666-6666'},
+		{name: 'André', phone: '7777-7777'}
+	];
+
+	$scope.operators = [
+		{name: 'Oi', code: 14, category: 'Fixed'},
+		{name: 'Vivo', code: 15, category: 'Mobile'},
+		{name: 'Tim', code: 41, category: 'Mobile'}
+	];
+
+	$scope.addContact = function(contact)
+	{
+		$scope.contacts.push(contact);
+		delete $scope.contact;
+	};
+
+	$scope.removeContact = function(contacts)
+	{
+		$scope.contacts = contacts.filter(function(contact)
+		{
+			if (!contact.selected){
+				return contact;
+			}
+		});
+	};
+
+	$scope.isContactSelected = function(contacts)
+	{
+		return contacts.some(function(contact)
+		{
+			return contact.selected;
+		});
+	}
+}]);
